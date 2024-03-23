eb2c9099aafcdcfde369f180860fbe030de24d88
Daniel Filho <danielfsf2001@gmail.com>
2024-03-23 17:00:29 -0300

    method get added

--- src/main/java/controller/ProspectController.java
+++ src/main/java/controller/ProspectController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("api/prospects")
+@RequestMapping("/")
 public class ProspectController {
     @GetMapping
     public String getProspects(){
