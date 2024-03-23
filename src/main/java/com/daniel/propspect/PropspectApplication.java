edaf082c623780d8dcdf6dcdd303dd3d41015b49
Daniel Filho <danielfsf2001@gmail.com>
2024-03-23 13:28:31 -0300

    initial commit

---
+++ src/main/java/com/daniel/propspect/PropspectApplication.java
@@ -0,0 +1,13 @@
+package com.daniel.propspect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PropspectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PropspectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/daniel/propspect/PropspectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.daniel.propspect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PropspectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
